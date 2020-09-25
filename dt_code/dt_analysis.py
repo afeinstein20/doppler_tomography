1560ea92f34639cd3f1957c6376be1029e8ca0bb
Adina Feinstein <adina.d.feinstein@gmail.com>
2020-09-25 12:54:17 -0500

    added the oot

--- dt_code/dt_analysis.py
+++ dt_code/dt_analysis.py
@@ -91,7 +91,7 @@ class DT(object):
             if useorders != 'all':
                 self.remove_bad_orders(np.array(useorders))
 
-            self.create_template(template_per_night)
+#            self.create_template(template_per_night)
 
 
     def load_files(self):
@@ -213,49 +213,145 @@ class DT(object):
             self.orders[i] = np.delete(self.orders[i], bad)
 
 
-    def create_template(self, template_per_night):
+    def find_out_of_transit(self):
         """
-        Creates a template per each order from each night.
+        Identifies which indices are outside of the transit.
 
-        Attributes
-        ----------
-        template_wave : np.ndarray
-        template_flux : np.ndarray
+        Returns
+        -------
+        days : np.ndarray
+             Rounded times to MJD days.
+        when_dict : dictionary
+             Indices of out-of-transit data for each unique 
+             night to make template from.
         """
         days = np.array(self.times.value, dtype=int)
 
+        when_dict = {}
+
+        for j, uo in enumerate(np.unique(days)):
+            out_inds = np.array([])
+
+            for i in range(len(self.ephemeris)):
+                arr = np.zeros(2)
+
+                multiperiods = np.arange(1,1000,1) * self.period[i].value + self.ephemeris[i].mjd
+                
+                start = multiperiods - self.duration[i].to(u.day).value/2
+                end   = multiperiods + self.duration[i].to(u.day).value/2
+
+                # Sees if the start of an event is within the night
+                try:
+                    arr[0] = start[np.where(np.array(start,dtype=int)==uo)[0]]
+                except:
+                    arr[0] = -100
+                    print("Start of object {1} transit begins before the night of {0} MJD.".format(uo, i))
+
+                # Sees if the end of an event is within the night
+                try:
+                    arr[1] = end[np.where(np.array(end,dtype=int)==uo)[0]]
+                except:
+                    arr[1] = -100
+                    print("Start of object {1} transit ends after the night of {0} MJD.".format(uo, i))
+
+                # Finds indices which are out of the night for the event
+                if arr[0] != -100 and arr[1] != -100:
+                    good_inds = np.where((self.times.value < arr[0]) | 
+                                         (self.times.value > arr[1]) )[0]
+                elif arr[0] == -100 and arr[1] != -100:
+                    good_inds = np.where(self.times.value > arr[1])[0]
+                elif arr[0] != -100 and arr[1] == -100:
+                    good_inds = np.where(self.times.value < arr[0])[0]
+
+                out_inds = np.append(out_inds, good_inds)
+
+            un, C = np.unique(out_inds, return_counts=True)
+            out_inds = un[C>1]
+            when_dict[uo] = out_inds
+
+        return when_dict
+
+
+    def create_template(self, ephemeris, duration, period,
+                        ephemeris_units='MJD', 
+                        duration_units=u.hr, template_per_night=True):
+        """
+        Creates a template per each order from each night.
+
+        Parameters
+        ----------
+        ephemeris : float, np.ndarray
+             Ephemeris/ephemerides of the object(s) transiting. This will be 
+             used to determine which frames are out of transit to make the
+             template.
+        duration : float, np.ndarray
+             Duration(s) of the object(s) transiting. This will be used to determine
+             which frames are out of transit to make the template.
+        period : float, np.ndarray
+             Period(s) [in days] for the object(s) transiting. This will be used to determine
+             which frames are out of the transit to make the template.
+        ephemeris_units : str, optional
+             The units the ephemeris/ephemerides is given in. Default is 'MJD'.
+             Other options include astropy.time.Time formats. More information can be
+             found here: https://docs.astropy.org/en/stable/time/#time-format.
+        duration_units : astropy.units.Unit
+             The units the duration is given in. Default is astropy.units.hour.
+        template_per_night : bool, optional              
+             Allows users to create a template per each night of observations or 
+             across all observations. Default is True.    
+
+        Attributes
+        ----------
+        ephemeris : astropy.time.Time
+        duration : astropy.units.Quantity
+        period : astropy.units.Quantity
+        template_wave : np.ndarray
+        template_flux : np.ndarray
+        when_event_dict : dictionary
+             The start and stop times for each event over a given night.
+        """
+        self.ephemeris = Time(ephemeris, format=ephemeris_units.lower())
+        self.duration  = np.array(duration) * duration_units
+        self.period = np.array(period) * u.day
+
+        when_dict = self.find_out_of_transit()
+
         if template_per_night == True:
-            where = np.where(np.diff(days)>=1)[0]
-            
-            if len(where) > 0:
-                per_day = np.append(where, where+1)
-                per_day = np.append(per_day, [0, len(days)-1])
-                per_day = per_day.reshape((int(len(per_day)/2),2))
-            else:
-                per_day = [np.array([0, len(days)-1])]
-
+            per_day = len(when_dict)
         else:
-            per_day = [np.array([0, len(days)-1])]
+            per_day = -1
 
-        template_wave = np.zeros((len(per_day), len(self.wave[0])))
-        template_flux = np.zeros((len(per_day), len(self.flux[0])))
-        template_flux_err = np.zeros((len(per_day), len(self.flux_err[0])))
-        template_orders = np.zeros((len(per_day), len(self.orders[0])))
+        template_wave     = np.zeros((np.abs(per_day), len(self.wave[0])))
+        template_flux     = np.zeros((np.abs(per_day), len(self.flux[0])))
+        template_flux_err = np.zeros((np.abs(per_day), len(self.flux_err[0])))
+        template_orders   = np.zeros((np.abs(per_day), len(self.orders[0])))
 
-        for i, pair in enumerate(per_day):
-            inds = np.arange(pair[0], pair[1]+1, 1, dtype=int)
+        if per_day == -1:
+            inds = np.array([], dtype=int)
+            for key in list(when_dict.keys()):
+                inds = np.append(inds, when_dict[key])
             template_wave[i] = np.nanmean(self.wave[inds], axis=0)
             template_flux[i] = np.nanmedian(self.flux[inds], axis=0)
             template_flux_err[i] = np.sqrt( np.nansum( self.flux_err[inds]**2))/len(inds)
             template_orders[i] = np.nanmedian(self.orders[inds])
 
+        else:
+            for i in range(per_day):
+                inds = np.array(when_dict[list(when_dict.keys())[i]], dtype=int)
+                template_wave[i] = np.nanmean(self.wave[inds], axis=0)
+                template_flux[i] = np.nanmedian(self.flux[inds], axis=0)
+                template_flux_err[i] = np.sqrt( np.nansum( self.flux_err[inds]**2))/len(inds)
+                template_orders[i] = np.nanmedian(self.orders[inds])
+
+
+        ## THIS NEEDS TO BE TESTED FOR MULTI-DAY DATA SETS :D
+
         self.template_wave = template_wave
         self.template_flux = template_flux
         self.template_flux_err = template_flux_err
         self.template_orders = template_orders
+        self.when_event_dict = when_dict
             
-
-
 
     def load_lines(self, filename, path=None, indices=[0,1],
                    units=u.Angstrom, delimiter=','):
